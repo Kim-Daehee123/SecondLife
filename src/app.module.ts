76f0d1401d505172d58fa20d39ce0d1e081bdaf7
Kim-Daehee123 <rkqkdxma@naver.com>
2022-06-15 17:03:11 +0900

    환경설정

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_DATABASE,
+      multipleStatements: true,
+      entities: [], // 사용할 entity의 클래스명을 넣어둔다.
+      synchronize: false, // false로 해두는 게 안전하다.
+    }),
+    AuthModule,
+  ],
+})
+export class AppModule {}
